50a697e2467aca33326c335a8f9d09bc9634ccb3
Kalle Lilja <15094562+ThatKalle@users.noreply.github.com>
2023-03-14 01:50:59 +0100

    no errors

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,20 +21,20 @@ provider "azurerm" {
 #   name = var.az_rg_name
 # }
 
-resource "azurerm_key_vault" "bad_example" {
-  name                        = "test-asd-asd-ouhhearh12"
-  location                    = "West Europe"
-  resource_group_name         = "test-asd-rg"
-  enabled_for_disk_encryption = true
-  tenant_id                   = "123456ab-1a2b-3c45-67de-1234ab567cd8"
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+# resource "azurerm_key_vault" "bad_example" {
+#   name                        = "test-asd-asd-ouhhearh12"
+#   location                    = "West Europe"
+#   resource_group_name         = "test-asd-rg"
+#   enabled_for_disk_encryption = true
+#   tenant_id                   = "123456ab-1a2b-3c45-67de-1234ab567cd8"
+#   soft_delete_retention_days  = 7
+#   purge_protection_enabled    = false
 
-  sku_name = "standard"
-}
+#   sku_name = "standard"
+# }
 
-resource "azurerm_key_vault_secret" "bad_example" {
-  name         = "secret-sauce"
-  value        = "szechuan"
-  key_vault_id = azurerm_key_vault.bad_example.id
-}
+# resource "azurerm_key_vault_secret" "bad_example" {
+#   name         = "secret-sauce"
+#   value        = "szechuan"
+#   key_vault_id = azurerm_key_vault.bad_example.id
+# }
