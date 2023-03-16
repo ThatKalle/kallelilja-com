a12608399140beef5d34fa9fc4e42d7e3b4583da
Kalle Lilja <15094562+ThatKalle@users.noreply.github.com>
2023-03-16 20:34:40 +0100

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.4.0"
+  required_version = "~> 1.4.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.47.0"
+      version = "~> 3.47.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "=3.4.3"
+      version = "~> 3.4.3"
     }
   }
 }
