4c11c132cca388d0600d00e65e71db030d8e5386
Kalle <15094562+ThatKalle@users.noreply.github.com>
2025-02-24 18:32:13 +0000

    update dns

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -7,23 +7,23 @@
 #   }
 # }
 
-# resource "dns_a_record_set" "kallelilja-com" {
+# resource "dns_a_record_set" "kallelilja_com" {
 #   zone      = "kallelilja.com."
 #   name      = "@"
 #   addresses = var.dns_ghpages_a_records
 #   ttl       = var.dns_default_ttl
 # }
 
-# resource "dns_aaaa_record_set" "kallelilja-com" {
+# resource "dns_aaaa_record_set" "kallelilja_com" {
 #   zone      = "kallelilja.com."
 #   name      = "@"
 #   addresses = var.dns_ghpages_aaaa_records
 #   ttl       = var.dns_default_ttl
 # }
 
-# resource "dns_cname_record" "www-kallelilja-com" {
+# resource "dns_cname_record" "www_kallelilja_com" {
 #   zone  = "kallelilja.com."
 #   name  = "www"
-#   cname = "kallelilja.com."
+#   cname = "thatkalle.github.io."
 #   ttl   = var.dns_default_ttl
 # }
