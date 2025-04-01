a643f4aa7c7085cb778c75d50b30ac5d434ead00
Kalle <15094562+ThatKalle@users.noreply.github.com>
2025-04-01 13:34:52 +0000

    add validation

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "branch_names" {
-  description = "List of branch names"
+  description = "List of branch names."
   type        = set(string)
   default     = ["main"]
 }
@@ -17,14 +17,19 @@ variable "statuscake_default_statuscodes" {
 
 #   validation {
 #     condition     = can(base64decode(var.dns_key_secret))
-#     error_message = "The dns_key_secret value must be a valid Base64-encoded string."
+#     error_message = "The 'dns_key_secret' value must be a valid Base64-encoded string."
 #   }
 # }
 
 # variable "dns_default_ttl" {
 #   description = "The TTL of the record."
 #   type        = number
-#   default     = 900
+#   default     = 3600
+
+#   validation {
+#     condition     = var.dns_default_ttl <= 604800
+#     error_message = "The highest possible DNS TTL value is 604800 (7 days)."
+#   }
 # }
 
 # variable "dns_ghpages_a_records" {
@@ -36,6 +41,13 @@ variable "statuscake_default_statuscodes" {
 #     "185.199.110.153",
 #     "185.199.111.153"
 #   ]
+
+#   validation {
+#     condition = alltrue([
+#       for ip in var.dns_ghpages_a_records : can(regex("^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$", ip))
+#     ])
+#     error_message = "All items in 'dns_ghpages_a_records' must be valid IPv4 addresses."
+#   }
 # }
 
 # variable "dns_ghpages_aaaa_records" {
@@ -47,4 +59,11 @@ variable "statuscake_default_statuscodes" {
 #     "2606:50c0:8002::153",
 #     "2606:50c0:8003::153"
 #   ]
+
+#   validation {
+#     condition = alltrue([
+#       for ip in var.dns_ghpages_aaaa_records : can(regex("^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$|^([0-9a-fA-F]{1,4}:){1,7}:$|^([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}$|^([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}$|^([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}$|^([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}$|^([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}$|^[0-9a-fA-F]{1,4}(:[0-9a-fA-F]{1,4}){1,6}$", ip))
+#     ])
+#     error_message = "All items in 'dns_ghpages_aaaa_records' must be valid IPv6 addresses."
+#   }
 # }
