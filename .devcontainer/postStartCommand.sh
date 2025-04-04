c9af30039376dc7fd5977b0997a4a870b5579727
Kalle <15094562+ThatKalle@users.noreply.github.com>
2025-04-04 11:28:13 +0000

    update version information

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -4,22 +4,19 @@ git submodule update --init
 
 cd terraform/
 tflint --init
-
-terraform --version
-tflint --version
-trivy --version
-tfprovidercheck --version
 cd ..
 
-cd .github/workflows
-actionlint --version
-shellcheck --version
-cd ../..
+# Print version information
+echo "Terraform: $(terraform --version | awk 'NR==1 {print $2}')"
+echo "TFLint: $(tflint --version | awk 'NR==1 {print $3}')"
+echo "tfprovidercheck: $(tfprovidercheck --version | awk 'NR==1 {print $3}')"
+echo "Trivy: $(trivy --version | awk 'NR==1 {print $2}')"
+echo "Actionlint: $(actionlint --version | awk 'NR==1 {print $1}')"
+echo "ShellCheck: $(shellcheck --version | awk 'NR==2 {print $2}')"
+echo "Dockerize: $(dockerize --version)"
+echo "Hugo: $(hugo version | awk 'NR==1 {print $2}')"
 
 cd web/
-dockerize --version
-hugo version
-
 if [ -f "nohup.out" ]; then rm nohup.out; fi
 nohup hugo server --baseURL localhost --poll 500ms & dockerize -wait tcp://localhost:1313 -timeout 30s
 cd ..
