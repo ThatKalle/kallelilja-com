ef0a47d30fe047f1c77fa8ebe8f4b3d2be525bd9
Kalle <15094562+ThatKalle@users.noreply.github.com>
2025-04-17 16:55:00 +0000

    remove dockerize

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -16,8 +16,3 @@ echo "ShellCheck: $(shellcheck --version | awk 'NR==2 {print $2}')"
 echo "Dockerize: $(dockerize --version)"
 echo "Hugo: $(hugo version | awk 'NR==1 {print $2}')"
 echo "Bats: $(bats --version | awk 'NR==1 {print $2}')"
-
-cd web/
-if [ -f "nohup.out" ]; then rm nohup.out; fi
-nohup hugo server --baseURL localhost --poll 500ms & dockerize -wait tcp://localhost:1313 -timeout 30s
-cd ..
